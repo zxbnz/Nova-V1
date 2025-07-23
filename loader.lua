dce4fb191035d9bb8fece7536a98ad418bbfa12c
zxbnz <159658374+zxbnz@users.noreply.github.com>
2025-07-23 14:50:47 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -67,10 +67,10 @@ end
 
 
 -- Buttons
-makeButton("ValoBlox", 40, "https://raw.githubusercontent.com/zxbnz/Valolo-Blox/refs/heads/main/code.lua")
-makeButton("Jailbird", 80, "https://raw.githubusercontent.com/zxbnz/JailBird-Cheat/refs/heads/main/code.lua")
-makeButton("Operation Siege", 120, "https://raw.githubusercontent.com/zxbnz/OS-Cheat/refs/heads/main/code.lua")
-makeButton("Counter Blox", 160, "https://raw.githubusercontent.com/zxbnz/CounterBlox/refs/heads/main/script.lua")
+makeButton("ValoBlox", 40, "https://raw.githubusercontent.com/zxbnz/Nova-V1/refs/heads/main/Valoblox.lua")
+makeButton("Jailbird", 80, "https://raw.githubusercontent.com/zxbnz/Nova-V1/refs/heads/main/Jailbird.lua")
+makeButton("Operation Siege", 120, "https://raw.githubusercontent.com/zxbnz/Nova-V1/refs/heads/main/OS.lua")
+makeButton("Counter Blox", 160, "https://raw.githubusercontent.com/zxbnz/Nova-V1/refs/heads/main/Counterblox.lua")
 
 
 -- Dragging
